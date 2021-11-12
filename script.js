d6304648d2d59f57f9446c0a1dab927ff4d99c5b
dokaubrys <kaubrysdovydas@gmail.com>
2021-11-12 17:36:25 +0200

    sd

---
+++ script.js
@@ -0,0 +1,13 @@
+function showMyDiv() {
+    document.getElementById("labelDiv").style.display = "contents";
+    document.getElementById("salaryDiv").style.display = "none";
+
+   
+}
+
+function hideMyDiv() {
+    document.getElementById("labelDiv").style.display = "none";
+    document.getElementById("salaryDiv").style.display = "contents";
+}
+
+
